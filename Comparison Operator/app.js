e73a0fa554bff5e40ecc4c720c8d54593a0fa8f6
Faraz5658 <farazahmad5623@gmail.com>
2025-07-23 22:53:01 +0500

    comparision

---
+++ Comparison Operator/app.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 20;
+console.log(a>b);
+
+let c = 10;
+let d = 20;
+console.log(a<b);
+
+let e = 10;
+let f = 10;
+console.log(a>=b);
+
+let g = 20;
+let h = 20;
+console.log(a>=b);
+
+let i = 10;
+let j = 20;
+console.log(a==b);
+
+let k = 10;
+let l = 20;
+console.log(a!=b);
+
