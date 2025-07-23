a0c9757134d27b6806668b29bf089aace8b6c41d
Faraz5658 <farazahmad5623@gmail.com>
2025-07-23 22:52:32 +0500

    +-*/%

---
+++ Arithmetic Operator/app.js
@@ -0,0 +1,25 @@
+let a = 24;
+let b = 12;
+
+let addition = a+b;
+console.log('addition: ', addition);
+
+let subtration = a-b;
+console.log('subtration: ', subtration);
+
+
+let muliply = a*b;
+console.log('muliply: ', muliply);
+
+
+let division = a/b;
+console.log('division: ', division);
+
+
+let exponentiation = a**5;
+console.log('exponentiation: ', exponentiation);
+
+
+
+let moduls = a%b;
+console.log('moduls: ', moduls);
